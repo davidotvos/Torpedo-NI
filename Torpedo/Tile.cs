ea535632ea79f66cac67f36f754918ad075d848a
davidotvos <davidotvos@wtf.com>
2022-05-27 12:16:38 +0200

    Index,Row,Col és Tile attribútumok hozzáadása

--- Torpedo/Tile.cs
+++ Torpedo/Tile.cs
@@ -10,13 +10,36 @@ namespace Torpedo
 {
     public class Tile : BindableBase
     {
+        // Tábla koordináták
         private int _index;
         private int _row;
         private int _col;
+
+        // Üres Cella
         private bool _isSea;
+
+        // Hajók állapotához
         private bool _isHit;
         private bool _isSunk;
 
+
+        public int Index
+        {
+            get { return _index; }
+            set { SetProperty(ref _index, value); }
+        }
+
+        public int Row 
+        {
+            get { return _row; } 
+            set { SetProperty(ref _row, value); } 
+        }
+
+        public int Col
+        {
+            get { return _col; }
+            set { SetProperty(ref _col, value); }
+        }
 
     }
 }
