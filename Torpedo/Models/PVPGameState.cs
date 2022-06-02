579b8a4ffbf2f7586238e02da29a27e1ee397446
davidotvos <davidotvos@wtf.com>
2022-06-02 18:49:31 +0200

    model, Player,Ship, PVPGameState, AIGameState added

---
+++ Torpedo/Models/PVPGameState.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torpedo.Models
+{
+    public class PVPGameState
+    {
+        private string _player1 { get; set; }
+        private string _player2 { get; set; }
+
+        private int[] _player1moves { get; set; }
+        private int[] _player2moves { get; set; }
+
+        private List<Tile> _player1board { get; set; }
+        private List<Tile> _player2board { get; set; }
+
+        private string _winner { get; set; }
+        private int _round { get; set; }
+
+        private int _player1hits { get; set; }
+        private int _player2hits { get; set; }
+
+        private List<Ship> _player1ships { get; set; }
+        private List<Ship> _player2ships { get; set; }
+
+        public void AddMove(int move)
+        {
+
+        }
+
+
+
+
+    }
+}
---
+++ Torpedo/Models/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torpedo.Models
+{
+    public class Player
+    {
+        private string _name { get; set; }
+        private int[] _moves { get; set; }
+        private List<Tile> _board { get; set; }
+        private int _score { get; set; }
+        private int _hits { get; set; }
+        private List<Ship> _ships { get; set; }
+    }
+}
