d5397ad3270805f491f6bb78d3ffea09e7263ca3
davidotvos <davidotvos@wtf.com>
2022-06-02 19:13:37 +0200

    Model modifications

--- Torpedo/Models/PVPGameState.cs
+++ Torpedo/Models/PVPGameState.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,33 +7,28 @@ using System.Threading.Tasks;
 
 namespace Torpedo.Models
 {
-    public class PVPGameState
+    public class PVPGameState : BindableBase
     {
-        private string _player1 { get; set; }
-        private string _player2 { get; set; }
+        private Player _player1;
+        private Player _player2;
 
-        private int[] _player1moves { get; set; }
-        private int[] _player2moves { get; set; }
+        private int _rounds;
 
-        private List<Tile> _player1board { get; set; }
-        private List<Tile> _player2board { get; set; }
-
-        private string _winner { get; set; }
-        private int _round { get; set; }
-
-        private int _player1hits { get; set; }
-        private int _player2hits { get; set; }
-
-        private List<Ship> _player1ships { get; set; }
-        private List<Ship> _player2ships { get; set; }
-
-        public void AddMove(int move)
+        public int Rounds
         {
-
+            get { return _rounds; }
+            set { SetProperty(ref _rounds, value); }
+        }
+        public Player Player1
+        {
+            get { return _player1; }
+            set { SetProperty(ref _player1, value); }
         }
 
-
-
-
+        public Player Player2
+        {
+            get { return _player2; }
+            set { SetProperty(ref _player2, value); }
+        }
     }
 }
--- Torpedo/Models/Player.cs
+++ Torpedo/Models/Player.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,13 +7,60 @@ using System.Threading.Tasks;
 
 namespace Torpedo.Models
 {
-    public class Player
+    public class Player : BindableBase
     {
-        private string _name { get; set; }
-        private int[] _moves { get; set; }
-        private List<Tile> _board { get; set; }
-        private int _score { get; set; }
-        private int _hits { get; set; }
-        private List<Ship> _ships { get; set; }
+        private string _name;
+        private int[] _moves;
+        private List<Tile> _board;
+        private int _score;
+        private int _hits;
+        private List<Ship> _ships;
+
+        public string Name
+        {
+            get { return _name; }
+            set { SetProperty(ref _name, value); }
+        }
+
+        public int[] Moves
+        {
+            get { return _moves; }
+            set { SetProperty(ref _moves, value); }
+        }
+
+        public List<Tile> Boards 
+        { 
+            get { return _board; }
+            set { SetProperty(ref _board, value); }
+        }
+
+        public int Score
+        {
+            get { return _score; }
+            set { SetProperty(ref _score, value); }
+        }
+
+        public int Hits
+        {
+            get { return _hits; }
+            set { SetProperty(ref _hits, value); }
+        }
+
+        public List<Ship> Ships
+        {
+            get { return _ships; }
+            set { SetProperty(ref _ships, value); }
+        }
+
+        public bool AddMove(int move)
+        {
+            if(move < 0 || move > 100)
+            {
+                return false;
+            }
+
+            _moves.Append(move);
+            return true;
+        }
     }
 }
